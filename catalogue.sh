1af79d75a1b379ec929bf4860d5b87529301d68f
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-29 00:15:03 -0600

    Configuring Catalogue Instance

---
+++ catalogue.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+MONGODB_HOST=mongodb.dawgs.online
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+#### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling Nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing Nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating USER"
+mkdir /app 
+VALIDATE $? "Creating App Dir"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Catalogue Application"
+cd /app 
+VALIDATE $? "Changing to App Dir"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzip Catalogue"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Install Dep" 
+cp catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Systemctl Service"
+systemctl daemon-reload
+VALIDATE $? "Daemon Reload"
+
+systemctl enable catalogue
+VALIDATE $? "Enable Catalogue" 
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Cpoying Mongo Repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing Mongo Client"
+mongosh --host $MONGODB_HOST </app/db/master-data.js
+VALIDATE $? "Load catalogue Products"
+
+systemctl restart catalogue
+VALIDATE $? "Restarted Catalogue"
