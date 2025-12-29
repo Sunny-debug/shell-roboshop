0d6a83df7699d274daf61d55df11aa67ec4555b0
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-28 18:59:12 -0600

    Added Route 53 records

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,8 @@ set -e
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0e25d883f9f15d071"
-
+ZONE_ID="Z0095538Q74XD6XU6M0Z"
+DOMAIN_NAME="dawgs.online"
 for instance in "$@"
 do
   InstanceID=$(
@@ -24,12 +25,33 @@ do
       --instance-ids "$InstanceID" \
       --query 'Reservations[0].Instances[0].PrivateIpAddress' \
       --output text)
+      RECORD_NAME="$instance.$DOMAIN_NAME"
   else
     IP=$(aws ec2 describe-instances \
       --instance-ids "$InstanceID" \
       --query 'Reservations[0].Instances[0].PublicIpAddress' \
       --output text)
+      RECORD_NAME="$DOMAIN_NAME"
   fi
 
   echo "$instance:$IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
