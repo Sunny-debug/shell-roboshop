e1d2608b4174098874fa0f2ae7d087e5fdbfce14
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-29 22:03:16 -0600

    Setup Redis

---
+++ redis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Cmd is :: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+
+dnf module enable redis:7 -y
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? 'Redis Installed'
+
+sed -i '127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+sed -i 's/^protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis/redis.conf
+
+systemctl enable redis
+
+systemctl start redis
+VALIDATE $? ' Successful '
